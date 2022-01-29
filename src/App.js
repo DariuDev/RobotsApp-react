bddb2b3f84161903d819b4b0359dcbf4ec00f2a5
dariush <dariushtaghavi9797@gmail.com>
2022-01-29 10:10:35 +0330

    add search box

--- src/App.js
+++ src/App.js
@@ -2,13 +2,30 @@ import React from 'react';
 import {robots} from './robots';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-const App = () => {
-    return(
-    <div className= 'tc '>
-        <h1>RoboFriends</h1>
-    <SearchBox/>
-    <CardList robots ={robots}/> 
-    </div>
-    );
+
+
+ class App extends React.Component {
+     constructor(){
+         super();
+          this.state ={
+             robots : robots,
+             searchFeild : ''
+         }
+     }
+     onSearchChange = (event) => {
+         this.setState({searchFeild : event.target.value})
+         }
+    render(){
+        const filterRobos = this.state.robots.filter(robots => {
+            return robots.name.toLowerCase().includes(this.state.searchFeild.toLowerCase());
+           })  
+        return(
+            <div className= 'tc '>
+                <h1>RoboFriends</h1>
+            <SearchBox seachChange = {this.onSearchChange}/>
+            <CardList robots ={filterRobos}/> 
+            </div>
+            );
+     }
 }
 export default App;
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-const SearchBox = () => {
+const SearchBox = (props) => {
     return(
         <div className ='pa2' >
 <input 
 className ='pa2 ba b--blue bg-lightest-blue grow tc'
+onChange = {props.seachChange}
 type='search'
  placeholder='search'/>
 </div>
